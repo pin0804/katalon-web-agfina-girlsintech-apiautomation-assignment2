01b9d5f291ef839bb8d02ee731b04296ed1ce576
agfina <agfinaa@gmail.com>
2023-08-14 00:37:49 +0700

    added obj and tc for GET-List Comments

--- Scripts/TC-Posts/TC-GET-List posts/Script1691942245301.groovy
+++ Scripts/TC-Posts/TC-GET-List posts/Script1691942245301.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WS.sendRequestAndVerify(findTestObject('posts/GET-List posts'))
+WS.sendRequestAndVerify(findTestObject('Posts/GET-List posts'))
 
-WS.sendRequestAndVerify(findTestObject('posts/GET-Single post'))
+WS.sendRequestAndVerify(findTestObject('Posts/GET-Single post'))
 
--- Scripts/TC-Posts/TC-POST-Posts/Script1691945904366.groovy
+++ Scripts/TC-Posts/TC-POST-Posts/Script1691945904366.groovy
@@ -17,5 +17,5 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WS.sendRequestAndVerify(findTestObject('posts/POST-Posts'))
+WS.sendRequestAndVerify(findTestObject('Posts/POST-Posts'))
 
